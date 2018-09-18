a49698cfbee2ef736bf6d1d3aaa87c5c835cc7a9
mars <mars@mars.com>
2018-09-18 18:20:28 +0800

    - npm winston

--- template/config/initializers/native/date.js
+++ template/config/initializers/native/date.js
@@ -1,4 +1,4 @@
 const moment = require('moment')
-Date.prototype.toString = function () {
-	return moment(this).format('YYYY-MM-DD HH:mm:ss')
+Date.prototype.toString = function (format = 'YYYY-MM-DD HH:mm:ss') {
+	return moment(this).format(format)
 }
