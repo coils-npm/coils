f8633e4b176750e4df509fa210799973349c267d
mars <mars@mars.com>
2018-09-22 22:46:50 +0800

    generate model tip Upcase

--- bin/coils-destroy-model.js
+++ bin/coils-destroy-model.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process')
 module.exports = function (model, attributes) {
+	console.assert(model.match(/^[A-Z]+/), 'Model Name do not allow lowercase')
 	exec(`sequelize model:generate --name ${model} --attributes ${attributes.join(',')} --underscored`, (err, stdout, stderr) => {
 		if (err) {
 			return console.error(err)
